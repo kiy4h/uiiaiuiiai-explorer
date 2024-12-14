20f9a79470f59b8e0f794f138bd42cc7546aa85a
kiyah <z4kiy4h.h@gmail.com>
2024-12-15 01:39:26 +0700

    filesystem.h: tidy up formatting

--- filesystem.h
+++ filesystem.h
@@ -4,45 +4,39 @@
 #include <cstdlib>
 #include <string>
 
-// TODO 改成项目的绝对路径（原来是用CMAKE生成的）
-const char* logl_root = "";
+const char *logl_root = "";
 
 class FileSystem {
 private:
-    typedef std::string (*Builder)(const std::string& path);
+    typedef std::string (*Builder)(const std::string &path);
 
 public:
-    static std::string getPath(const std::string& path)
-    {
-        static std::string (*pathBuilder)(std::string const&) = getPathBuilder();
+    static std::string getPath(const std::string &path) {
+        static std::string (*pathBuilder)(std::string const &) = getPathBuilder();
         return (*pathBuilder)(path);
     }
 
 private:
-    static std::string const& getRoot()
-    {
-		static char const* envRoot = getenv("LOGL_ROOT_PATH");
-        static char const* givenRoot = (envRoot != nullptr ? envRoot : logl_root);
+    static std::string const &getRoot() {
+        static char const *envRoot = getenv("LOGL_ROOT_PATH");
+        static char const *givenRoot = (envRoot != nullptr ? envRoot : logl_root);
         static std::string root = (givenRoot != nullptr ? givenRoot : "");
         return root;
     }
 
-    //static std::string(*foo (std::string const &)) getPathBuilder()
-    static Builder getPathBuilder()
-    {
+    // static std::string(*foo (std::string const &)) getPathBuilder()
+    static Builder getPathBuilder() {
         if (getRoot() != "")
             return &FileSystem::getPathRelativeRoot;
         else
             return &FileSystem::getPathRelativeBinary;
     }
 
-    static std::string getPathRelativeRoot(const std::string& path)
-    {
+    static std::string getPathRelativeRoot(const std::string &path) {
         return getRoot() + std::string("/") + path;
     }
 
-    static std::string getPathRelativeBinary(const std::string& path)
-    {
+    static std::string getPathRelativeBinary(const std::string &path) {
         return path;
     }
 };
