14ba7b209434515e8090b1d65ff185a6e2bbdcc6
kiyah <z4kiy4h.h@gmail.com>
2025-01-02 02:18:05 +0700

    fix depth issue

--- main.cpp
+++ main.cpp
@@ -79,6 +79,9 @@ int main() {
 
     // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
     stbi_set_flip_vertically_on_load(true);
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
 
     // build and compile shaders
     // -------------------------
